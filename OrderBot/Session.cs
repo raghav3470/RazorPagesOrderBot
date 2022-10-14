5786188f2be53d1c0cab1535ae6c82ba1f01dfc2
raghav3470 <115809427+raghav3470@users.noreply.github.com>
2022-10-14 11:54:21 -0400

    Update Session.cs

--- OrderBot/Session.cs
+++ OrderBot/Session.cs
@@ -24,8 +24,8 @@ namespace OrderBot
             switch (this.nCur)
             {
                 case State.WELCOMING:
-                    aMessages.Add("Welcome to Rich's Shawarama!");
-                    aMessages.Add("What size would you like?");
+                    aMessages.Add("Welcome to Royal Hotel .");
+                    aMessages.Add("How can i help you?");
                     this.nCur = State.SIZE;
                     break;
                 case State.SIZE:
